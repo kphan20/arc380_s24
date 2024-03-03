d7145b19c2c18fc5ac1c20a6134180c710502ea5
kphan20 <81403321+kphan20@users.noreply.github.com>
2024-03-03 15:44:49 -0500

    first implementation for part 1 and rectangle drawing

--- assignments/Assignment4/ARC380_Assignment4.py
+++ assignments/Assignment4/ARC380_Assignment4.py
@@ -21,6 +21,9 @@ def create_frame_from_points(point1: cg.Point, point2: cg.Point, point3: cg.Poin
     # ================================== YOUR CODE HERE ==================================
 
     # Part 1.c.
+    frame = cg.Frame.from_points(
+        point1, point2, point3
+    )  # TODO see if defining origin differently would help
 
     # ====================================================================================
     return frame
@@ -40,6 +43,7 @@ def transform_task_to_world_frame(ee_frame_t: cg.Frame, task_frame: cg.Frame) ->
     # ================================== YOUR CODE HERE ==================================
 
     # Part 1.d.
+    ee_frame_w = task_frame.to_world_coordinates(ee_frame_t)
 
     # ====================================================================================
     return ee_frame_w
@@ -48,6 +52,64 @@ def transform_task_to_world_frame(ee_frame_t: cg.Frame, task_frame: cg.Frame) ->
 # ====================== Drawing effects and helper functions ============================
 
 # Part 2
+
+
+class EETaskHandler:
+    def __init__(self, abb_rrc: rrc.AbbClient, task_frame: cg.Frame) -> None:
+        self.abb_rrc = abb_rrc
+        self.task_frame = task_frame
+
+    def lift_pen(self, dest: cg.Point):
+        """Helper function that lifts pen
+        when we want to move the pen but not draw"""
+
+        return cg.Point(
+            dest.x, dest.y, dest.z + 5
+        )  # TODO test values for lift, when to call this function
+
+    def move_to_point(self, dest: cg.Point, speed: float, quat: cg.Quaternion = None):
+        """Helper function that handles conversion to world frame and command sending"""
+
+        if quat is None:
+            frame = cg.Frame(dest, self.task_frame.xaxis, self.task_frame.yaxis)
+        else:
+            frame = cg.Frame.from_quaternion(quat, dest)
+
+        frame_w = transform_task_to_world_frame(frame, self.task_frame)
+        _ = self.abb_rrc.send_and_wait(
+            rrc.MoveToFrame(frame_w, speed, rrc.Zone.FINE, rrc.Motion.LINEAR)
+        )
+
+    def move_to_origin(self):
+        """Moves robot to task space origin"""
+        self.move_to_point(cg.Point(0, 0), 10)
+
+    def draw_rectangle(
+        self, center: cg.Point, length: float, width: float, theta: float
+    ):
+        """center is in task frame, theta is rotation around task frame z axis"""
+
+        # construct rectangle corner points
+        hl, hw = length / 2, width / 2
+        top_left = cg.Point(center.x - hw, center.y + hl)
+        top_right = cg.Point(center.x + hw, center.y + hl)
+        bot_right = cg.Point(center.x + hw, center.y - hl)
+        bot_left = cg.Point(center.x - hw, center.y - hl)
+        corners = [top_left, top_right, bot_right, bot_left]
+
+        # rotate each point around the rectangle's center
+        rotation = cg.Rotation.from_axis_and_angle(cg.Vector.Zaxis, theta, center)
+        cg.Point.transform_collection(corners, rotation)
+
+        # move pen to corners and draw when appropriate
+        speed = 30
+        self.move_to_point(self.lift_pen(corners[0]), speed)
+
+        for corner in corners:
+            self.move_to_point(corner, speed)
+
+        self.move_to_point(corners[0])
+
 
 # ========================================================================================
 
@@ -63,6 +125,7 @@ if __name__ == '__main__':
     print('Connected.')
 
     # ================================== YOUR CODE HERE ==================================
+    abb_rrc.send(rrc.SetTool(""))  # TODO insert tool name
 
     # Parts 1.e. and 2
 
